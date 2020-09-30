0ddc0fe33e62ece55dcdd91def4bec03f8c049ec
Azri <59246759+katnaps@users.noreply.github.com>
2020-09-30 10:35:17 +0800

    First Commit via CLI

---
+++ precache-manifest.6b68d711ff56d7633053ff07ece9445c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb6c5b150a65b463124435433b9fa5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aec952fb4d04bedd995",
+    "url": "/static/css/main.81e65448.chunk.css"
+  },
+  {
+    "revision": "2893422eea20feffa622",
+    "url": "/static/js/2.83289916.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83289916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aec952fb4d04bedd995",
+    "url": "/static/js/main.301f0b4c.chunk.js"
+  },
+  {
+    "revision": "ca30c156207b57b4ef9a",
+    "url": "/static/js/runtime-main.b7132aff.js"
+  }
+]);
